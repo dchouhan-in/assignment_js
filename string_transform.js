337197043126f13a0498910c8e2d4000ba5ea31f
dichouhan <dichouhan@deqode.com>
2023-10-03 18:24:53 +0530

    feat: update and test code for string_transform.js

--- string_transform.js
+++ string_transform.js
@@ -1,9 +1,9 @@
 
 
-//  transforms string in following manner 
+//  transforms string in following manner - 
 // 1. reverse all the words
 // 3. lowercase vowels
-// 2. replace special characters with ascii index
+// 2. replace special characters with ascii index from 0
 // 4. join all the words into a single string, using pipe ("|")
 
 
@@ -13,19 +13,27 @@ function reverseString(string) {
 }
 
 function transformString(string) {
-    let split_str = string.split(" ")
-    split_str.map((value) => {
+    let splitStr = string.split(" ")
+    transformedArray = splitStr.map((value) => {
         let reverseStr = reverseString(value)
 
-        const vowelRegex = "/^[aeiou]$/i"
+        const vowelRegex = /[aeiou]/gi
 
-        reverseStr.replace(vowelRegex, (value) => {
-            value.toLowerCase()
+        reverseStr = reverseStr.replace(vowelRegex, (value) => {
+            return value.toUpperCase()
+        })
+
+        const specialCharacters = /[^A-Za-z 0-9]/gi
+
+        reverseStr = reverseStr.replace(specialCharacters, (value) => {
+            return value.charCodeAt(0) + "!"
         })
 
         return reverseStr
     })
 
+    return transformedArray.join("|")
+
 }
 
-console.log(transformString("this is a string!"))
+console.log(transformString("this is a string!@#"))
